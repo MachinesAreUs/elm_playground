bf40b284dd442d5c6f4481f27aaed07284d10362
Agustin Ramos <agustin.ramos@knowtion.biz>
2014-01-17 10:52:14 -0600

    Small refactor.

--- waves/waves.elm
+++ waves/waves.elm
@@ -10,8 +10,8 @@ ball hue = circle ballRadius |> filled (hsv hue 0.9 0.9)
 -- X points
 
 xPositions windowWidth itemSize = 
-  xPositions_ windowWidth (itemSize*2) [] |> map (\p -> p - (windowWidth / 2))
-
+  let adjustCoords x = x - (windowWidth / 2)
+  in xPositions_ windowWidth (itemSize*2) [] |> map adjustCoords
 
 xPositions_ remaining itemWidth positions = 
   let newPosition  = itemWidth * (length positions |> toFloat) + itemWidth
@@ -20,24 +20,19 @@ xPositions_ remaining itemWidth positions =
                                    newPosition :: positions                     
         | otherwise             -> reverse positions
 
--- Y points
+-- Math stuff
 
-yPositionsForX xcoords = 
-  let toYcoord x = sin (pi * x / periodLength) * amplitude
-  in map toYcoord xcoords
-
-thetaForX x = pi * x / periodLength
+pahseAngle p x = pi * x / p
+ypos a w       = (sin w) * a
 
 -- Main        
 
 scene (w,h) = 
-  let (wf, hf) = (toFloat w, toFloat h)
-      topLeft  = ballRadius - (wf/2)
-      xcoords  = xPositions wf (ballRadius * 2)
-      --ycoords  = yPositionsForX xcoords
-      angles   = map thetaForX xcoords
-      ypos w   = (sin w) * amplitude
-      toBall (x,w) = ball w |> move (x, ypos w)
+  let (wf, hf)     = (toFloat w, toFloat h)
+      topLeft      = ballRadius - (wf/2)
+      xcoords      = xPositions wf (ballRadius * 2)
+      angles       = map (pahseAngle periodLength) xcoords
+      toBall (x,w) = ball w |> move (x, ypos amplitude w)
   in collage w h <| map toBall <| zip xcoords angles
 
 main = lift scene Window.dimensions
