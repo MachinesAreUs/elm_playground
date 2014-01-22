9a180c0a192f2913432cc8737f45cc1204a78be2
Agustin Ramos <agustin.ramos@knowtion.biz>
2014-01-21 20:15:59 -0600

    Think I found an Elm bug. Need further investigation.

--- bubblespace/bubblespace.elm
+++ bubblespace/bubblespace.elm
@@ -17,6 +17,14 @@ text =
 
 -- Program
 
+scatter : (Int,Int) -> Int -> [Int] -> Form -> Form
+scatter (w,h) idx rands f =
+  let (wf,hf) = (toFloat w, toFloat h)
+      randsf  = map toFloat rands
+      [a,b]   = drop (idx * 2) randsf |> take 2 
+      pos     = (a * wf / 200, b * hf / 200)
+  in move pos f
+
 bubble r color = 
   let baseOutline = solid color
       outline     = { baseOutline | width <- lineWidth}
@@ -25,23 +33,41 @@ bubble r color =
     circle r             |> filled color |> alpha bubbleAlpha
   ] 
 
+
 scene {ttime, delta, rands, dim} =
-  let (w,h)     = dim
-      colors    = [red, orange, yellow, green, blue, purple]
-      toBubble  = bubble radius
+  let (w,h)                = dim
+      toBubble             = bubble radius
+      colors               = [red, orange, yellow, green, blue, purple]
+      unpositionedBubbles  = map toBubble colors
+      indexedBubbles       = zip unpositionedBubbles [0..10]
+      --bubbles              = map (\(f,idx) -> scatter (w,h) idx rands f) indexedBubbles
+      bubbles              = map fst indexedBubbles
   in collage w h 
-    (map toBubble colors)
+    bubbles
     |> color black
 
 type Input = { ttime: Time, delta: Time, rands: [Int], dim: (Int,Int) }
 
 input = 
-  let source = fps 30
+  let source = fps 1
       ttime  = foldp (+) 0 source
-      rands  = combine <| map (\_ -> Random.range 0 100 source) [1..100] 
+      rands  = combine <| map (\_ -> Random.range -100 100 source) [1..100] 
   in sampleOn ttime <| Input <~ ttime
                               ~ (inSeconds <~ source)
                               ~ rands
                               ~ Window.dimensions
 
 main = lift scene input
+
+{-scatter : (Int,Int) -> Int -> [Int] -> Form -> Form
+scatter (w,h) idx rands f =
+  let randsf  = map toFloat rands
+      (wf,hf) = (toFloat w, toFloat h)
+      [a,b]   = drop (idx * 2) randsf |> take 2 
+      pos     = (a * wf / 200, b * hf / 200)
+  in move pos f
+
+main = collage 400 400 [
+    circle 30 |> filled blue |> scatter (400,400) 0 [50,-50,2,50,1,90]
+  ]
+-}
