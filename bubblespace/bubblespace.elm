eb68148ea33db6bf4c25200ff14d1adf6fbf13cc
Agustin Ramos Fonseca <zentimental.software@gmail.com>
2015-07-22 20:09:26 -0500

    Almost finished moving bubblespace to elm 0.15.1

--- bubblespace/bubblespace.elm
+++ bubblespace/bubblespace.elm
@@ -1,8 +1,12 @@
-import Window
-import Graphics.Collage 
-import Random
-import Text
-import Signal
+import Window exposing (..)
+import Random exposing (..)
+import Signal exposing (..)
+import Text exposing (..)
+import Time exposing (..)
+import List exposing (..)
+import Color exposing (..)
+import Graphics.Collage exposing (..)
+import Graphics.Element exposing (..)
 
 radius      = 30
 lineWidth   = 3
@@ -13,14 +17,20 @@ bubbleAlpha = 0.4
 (!!) list idx = drop idx list |> head
 
 text = 
-  toForm . Text.text . (Text.color white) . toText . show 
+  Graphics.Collage.text << (Text.color white) << fromString << toString
+
+-- This function was removed form Signal package
+-- See: https://github.com/elm-lang/core/commit/e7c5c4c57850867bf46ac267cde091b391fefb26
+
+combine : List (Signal a) -> Signal (List a)
+combine = List.foldr (Signal.map2 (::)) (constant [])
 
 -- Program
 
-scatter : (Int,Int) -> Int -> [Int] -> Form -> Form
+scatter : (Int,Int) -> Int -> List Int -> Form -> Form
 scatter (w,h) idx rands f =
   let (wf,hf) = (toFloat w, toFloat h)
-      randsf  = map toFloat rands
+      randsf  = List.map toFloat rands
       [a,b]   = drop (idx * 2) randsf |> take 2 
       pos     = (a * wf / 200, b * hf / 200)
   in move pos f
@@ -37,22 +47,24 @@ scene {ttime, delta, rands, dim} =
   let (w,h)                = dim
       toBubble             = bubble radius
       colors               = [red, orange, yellow, green, blue, purple]
-      unpositionedBubbles  = map toBubble colors
-      indexedBubbles       = zip unpositionedBubbles [0..10]
-      bubbles              = map (\(f,idx) -> scatter (w,h) idx rands f) indexedBubbles
+      unpositionedBubbles  = List.map toBubble colors
+      indexedBubbles       = List.map2 (,) unpositionedBubbles [0..10]
+      --bubbles              = List.map (\(b,idx) -> scatter dim idx rands b) indexedBubbles
   in collage w h 
-    bubbles
-    |> color black
+    unpositionedBubbles
+    |> Graphics.Element.color black
 
-type Input = { ttime: Time, delta: Time, rands: [Int], dim: (Int,Int) }
+type alias Input = { ttime: Time, delta: Time, rands: List Int, dim: (Int,Int) }
 
 input = 
-  let source = fps 1
-      ttime  = foldp (+) 0 source
-      rands  = combine <| map (\_ -> Random.range -100 100 source) [1..100] 
+  let source     = fps 1
+      ttime      = foldp (+) 0 source
+      seed t     = initialSeed(floor(t))
+      randSignal = Signal.map (\s -> generate (int 0 100) (seed s) |> fst) ttime
+      rands      = combine <| List.map (\_ -> randSignal) [1..10]
   in sampleOn ttime <| Input <~ ttime
                               ~ (inSeconds <~ source)
                               ~ rands
                               ~ Window.dimensions
 
-main = lift scene input
+main = Signal.map scene input
