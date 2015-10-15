1e2ae6674b8a85d26d65dc5ff960c89ae9ede3a8
Agustín Ramos Fonseca <zentimental.software@gmail.com>
2015-10-15 14:53:27 -0500

    1) Improved reaction to user input. 2) Now using only forwared pipes

--- koch/koch2.elm
+++ koch/koch2.elm
@@ -7,6 +7,8 @@ import Color exposing (..)
 import Signal exposing (foldp, (~), (<~))
 import Time exposing (fps)
 import Keyboard
+
+iterations = 4
  
 middle (x,y) (x',y') = ((x + x') / 2, (y + y') / 2)
  
@@ -25,31 +27,31 @@ complete (x,y) (x',y') =
 kochSet n idx [p1,p5] =
   let (p2,p4) = trisect p1 p5
       p3      = complete p2 p4
-  in if | idx < n   -> concat <| map (kochSet n (idx + 1)) 
-                              <| [[p1,p2],[p2,p3],[p3,p4],[p4,p5]]
+  in if | idx < n   -> [[p1,p2],[p2,p3],[p3,p4],[p4,p5]]
+                         |> map (kochSet n (idx + 1)) 
+                         |> concat 
         | otherwise -> [[p1,p5]]
 
 koch (w,h) t theta = 
-  let iterations = 6
-      style      = filled (hsl (t * pi / 2000 ) 0.9 0.9) 
+  let style      = filled (hsl (t * pi / 1000 ) 0.9 0.8) 
       side       = 0.8 * min (toFloat w) (toFloat h)
       (dx,dy)    = (side / 2, tan (pi / 6) * side / 2)
       p1         = (-dx, -dy)
       p2         = (side * cos (pi / 3) - dx, side * sin (pi / 3) - dy)
       p3         = (dx, -dy)
       initialSet = [[p1,p2],[p2,p3],[p3,p1]]
-      heads      = \x -> case head x of
-                         Just p -> p
-                         _      -> (0,0)
-      curve      = style <| polygon
-                         <| map heads
-                         <| concat
-                         <| map (kochSet iterations 0)
-                         <| initialSet
-  in color black <| collage w h [ rotate theta curve ]
+      heads l    = l |> head |> Maybe.withDefault (0,0)
+      curve      = initialSet 
+                     |> map (kochSet iterations 0)
+                     |> concat
+                     |> map heads
+                     |> polygon
+                     |> style
+  in collage w h [ rotate theta curve ] |> color black
 
-time  = foldp (+) 0 (fps 2)
-angle = Signal.map toFloat <| foldp (\_ x -> x + 1) 0 Keyboard.space
+time       = foldp (+) 0 (fps 2)
+clickCount = foldp (\{x,y} c -> c + x) 0 Keyboard.arrows
+angle      = Signal.map toFloat clickCount
 
 main = koch <~ Window.dimensions 
              ~ time
